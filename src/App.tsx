d1f671f6e50d179119da02f2bda1c8bf93a97f22
Luke <101844513+luke-symes@users.noreply.github.com>
2025-03-19 19:24:29 +0000

    Set up basic page select input

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,12 @@ import { useState, useEffect } from 'react';
 import lodash from 'lodash';
 
 function App() {
-  const [tableData, setTableData] = useState<People>([]);
   const [isError, setIsError] = useState<boolean>(false);
   const [rowLimit, setRowLimit] = useState<number>(10);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [validPeopleData, setValidPeopleData] = useState<People | undefined>(
+    undefined,
+  );
 
   useEffect(() => {
     try {
@@ -23,7 +26,7 @@ function App() {
         setIsError(true);
         throw new ZodError(validation.error.issues);
       } else {
-        setTableData(peopleData);
+        setValidPeopleData(validation.data);
       }
     } catch (error: unknown) {
       if (error instanceof ZodError) {
@@ -36,16 +39,34 @@ function App() {
     }
   }, []);
 
+  function handleRowLimitSelectChange(rowLimit: string) {
+    setRowLimit(Number(rowLimit));
+  }
+
+  function handlePageSelectChange(page: string) {
+    setCurrentPage(Number(page));
+  }
+
   if (isError) {
     return <div>Error loading table data</div>;
   }
 
-  if (tableData.length > 0) {
+  if (validPeopleData && validPeopleData.length > 0) {
     const tableKeys = zPersonPickedKey.options;
-    const allRows: PersonPicked[] = tableData.map((person) =>
+    const allRows: PersonPicked[] = validPeopleData.map((person) =>
       lodash.pick(person, tableKeys),
     );
     const filteredRows = allRows.slice(0, rowLimit);
+
+    const visibleRows = filteredRows;
+
+    const totalPages = allRows.length / visibleRows.length;
+    const pageNumbers: number[] = [];
+
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(i);
+    }
+
     return (
       <>
         <label htmlFor="row-limit-select">Row limit:</label>
@@ -53,14 +74,29 @@ function App() {
           id="row-limit-select"
           name="Row limit"
           value={rowLimit}
-          onChange={(event) => setRowLimit(Number(event.target.value))}
+          onChange={(event) => handleRowLimitSelectChange(event.target.value)}
         >
           <option value="10">10</option>
           <option value="50">50</option>
           <option value="100">100</option>
           <option value="500">500</option>
         </select>
-        <PersonTable keys={tableKeys} rows={filteredRows} />
+        <label htmlFor="page-select">Page:</label>
+        <select
+          id="page-select"
+          name="Page"
+          value={currentPage}
+          onChange={(event) => handlePageSelectChange(event.target.value)}
+        >
+          {pageNumbers.map((pageNumber, index) => {
+            return (
+              <option key={index} value={String(pageNumber)}>
+                {pageNumber}
+              </option>
+            );
+          })}
+        </select>
+        <PersonTable keys={tableKeys} rows={visibleRows} />
       </>
     );
   }
