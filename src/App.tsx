3ffdff81e6e7ae8262edff3a637e390a0f32b28f
Luke <101844513+luke-symes@users.noreply.github.com>
2025-03-19 16:08:23 +0000

    Validate table data & handle error state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
 import './global.css';
+import { Table } from './components/Table';
+import { ctRoot as peopleData } from './data/random-people-data.json';
+import { type People, zPeople } from './schemas/person';
+import { ZodError } from 'zod';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return <></>;
+  let tableData: People | undefined;
+
+  const [isError, setIsError] = useState<boolean>(false);
+
+  useEffect(() => {
+    try {
+      const validation = zPeople.safeParse(peopleData);
+      if (!validation.success) {
+        setIsError(true);
+        throw new ZodError(validation.error.issues);
+      }
+    } catch (error: unknown) {
+      if (error instanceof ZodError) {
+        console.error('Zod Error validating people data:', error.issues);
+      } else if (error instanceof Error) {
+        console.error('Error validating people data', error.message);
+      } else {
+        console.error('Unknown error validating people data', error);
+      }
+    }
+  }, [tableData]);
+
+  if (isError) {
+    return <div>Error loading table data</div>;
+  }
+
+  if (tableData) return <Table></Table>;
 }
 
 export default App;
