6748874df90321e2bcec5f89f34a377c97abdf8c
Luke <101844513+luke-symes@users.noreply.github.com>
2025-03-19 18:17:56 +0000

    Export picked person schema type

--- src/schemas/person.ts
+++ src/schemas/person.ts
@@ -34,5 +34,6 @@ export const zPersonPickedKey = zPersonPicked.keyof();
 
 export type Person = z.infer<typeof zPerson>;
 export type People = z.infer<typeof zPeople>;
+export type PersonPicked = z.infer<typeof zPersonPicked>;
 export type PersonKey = z.infer<typeof zPersonKey>;
 export type PersonPickedKey = z.infer<typeof zPersonPickedKey>;
