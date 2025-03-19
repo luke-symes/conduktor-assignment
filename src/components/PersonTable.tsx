978352b0f3ea99280711a70c7813a7325ff5da7e
Luke <101844513+luke-symes@users.noreply.github.com>
2025-03-19 18:17:07 +0000

    Rename table component & render values

---
+++ src/components/PersonTable.tsx
@@ -0,0 +1,45 @@
+import { type PersonPickedKey, type PersonPicked } from '../schemas/person';
+
+interface TableProps {
+  keys: PersonPickedKey[];
+  rows: PersonPicked[];
+}
+
+interface RowProps {
+  data: PersonPicked;
+}
+
+export function PersonTable({ keys, rows }: TableProps) {
+  return (
+    <table className="table-auto">
+      <thead>
+        <tr>
+          {keys.map((key) => {
+            return (
+              <th className="capitalize" key={key}>
+                {key}
+              </th>
+            );
+          })}
+        </tr>
+      </thead>
+
+      <tbody>
+        {rows.map((row, index) => {
+          return <Row data={row} key={index} />;
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+function Row({ data }: RowProps) {
+  const values = Object.values(data);
+  return (
+    <tr>
+      {values.map((value, index) => (
+        <td key={index}>{String(value)}</td>
+      ))}
+    </tr>
+  );
+}
